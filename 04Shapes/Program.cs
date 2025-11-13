6d556e26292b6b22dc9630ba354a4dec5222b345
Дмитрий Евстифеев <Runkas4@gmail.com>
2025-11-13 19:07:44 +0300

    04 - Наследование

---
+++ 04Shapes/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyApp
+{
+    class Shape
+    {
+        public virtual void Draw()
+        {
+            Console.WriteLine("Рисуем фигуру");
+        }
+    }
+    class Circle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Рисуем круг");
+        }
+    }    class Square : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Рисуем квадрат");
+        }
+    }
+class Program
+    {
+        static void Main(string[] args)
+        {
+            List < Shape > Shape = new List<Shape>();
+            Shape.Add(new Circle());
+            Shape.Add(new Square());
+            foreach (Shape shape in Shape){
+                shape.Draw();
+            }
+        }
+    }
+}

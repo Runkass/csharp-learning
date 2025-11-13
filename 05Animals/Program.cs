3797e8c041beab10069b00bc606eb77a5e7300eb
Дмитрий Евстифеев <Runkas4@gmail.com>
2025-11-13 19:55:33 +0300

    05 - абстрактные классы

---
+++ 05Animals/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AbstractAnimals
+{
+    abstract class Animal
+    {
+        public abstract void MakeSound();
+    }
+    class Dog : Animal
+    {
+        public override void MakeSound() => Console.WriteLine("Гав");
+    }
+    class Cat : Animal
+    {
+        public override void MakeSound()=> Console.WriteLine("Мяу");
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Animal> animals = new List<Animal>();
+            animals.Add(new Cat());
+            animals.Add(new Dog());
+            foreach (Animal animal in animals)
+            {
+                animal.MakeSound();
+            }
+        }
+    }
+}
